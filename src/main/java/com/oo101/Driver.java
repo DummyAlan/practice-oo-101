a190480a34fa015535007cb0a385d8eff2a0c6c4
Alan Wan <alan.wan@oocl.com>
2022-11-30 16:15:06 +0800

    Feat: finished AC2

--- src/main/java/com/oo101/Driver.java
+++ src/main/java/com/oo101/Driver.java
@@ -6,6 +6,9 @@ public class Driver {
     public static void main(String[] args){
         Car coolCar = new Car("Cool Car", 30);
         coolCar.speedUp();
+        Truck bigTruck = new Truck("Big Truck", 10);
+        bigTruck.speedUp();
+
     }
 
 
---
+++ src/main/java/com/oo101/Truck.java
@@ -0,0 +1,15 @@
+package com.oo101;
+
+public class Truck extends Vehicle{
+
+
+    public Truck(String name, int speed) {
+        super(name, speed);
+    }
+
+    @Override
+    public void speedUp() {
+        super.speedUp();
+        System.out.println(this.name + ": speed up "+this.speed+" km/h");
+    }
+}
